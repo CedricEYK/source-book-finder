5a137531ad01c80177325f342fb12b8bd1e38634
Cedric.K <cedric.e.y.k@hotmail.com>
2023-02-18 01:05:52 +0100

    hot fix mainroutes removal

--- src/backend/src/routes/mainRoutes.js
+++ src/backend/src/routes/mainRoutes.js
@@ -1,12 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const mainControllers = require('../controllers/mainControllers');
 const scrapeCtrl = require('../controllers/getArticleBooks');
-
-router.get('/', mainControllers.getMainPage);
-
-//router.post('/search', mainControllers.postSearch);
 
 router.post('/search', scrapeCtrl.scrapeArticle);
 
